99f085434071b66acfb65bbabeeaf9a5bc85882b
node-rookie <749924911@qq.com>
2015-12-02 17:09:15 +0800

    update

--- src/app/index.js
+++ src/app/index.js
@@ -12,54 +12,9 @@ util.mixin(app['nodeManager'], new NodeManager());
 
 app.start(callback);
 
-function callback(){
+function callback() {
     console.log('[system]: VN is startup!');
-    getBroker.then(function(broker){
-        /**
-         *  msg: {
-         *      CreateTime: Number: Date#getTime() milliseconds
-         *      NodeId: String
-         *      RAM: String
-         *      CPU: String
-         *      ExceptedAgentSum: 预计
-         *      ActualAgentSum: 实际
-         *  }
-         */
-        broker.brokerNodeManager.onAgentManagerStatusResponse(function(err, data){
-            console.log("[system]: agent manager status response");
-            console.log(data);
-            app.nodeManager.nodeMaps[data.NodeId] = {
-                RAM: data.RAM,
-                CPU: data.CPU,
-                exceptedAgentSum: data.ExceptedAgentSum,
-                actualAgentSum: data.ActualAgentSum
-            };
-
-        });
-        /**
-         *  msg: {
-         *      CreateTime: Number: Date#getTime() milliseconds
-         *      NodeId: String
-         *      AgentId: String
-         *      AgentStatus: String
-         *       - starting
-         *       - logging
-         *       - mislogged
-         *       - logged
-         *       - exceptional
-         *       - aborted
-         *       - exited
-         *  }
-         */
-        broker.brokerNodeManager.onAgentStatusResponse(function(err, data){
-            console.log("[system]: agent status response");
-            console.log(data);
-            app.nodeManager.agentMaps[data.AgentId] = {
-                agentId: data.AgentId,
-                nodeId: data.NodeId,
-                agentStatus: data.AgentStatus
-            };
-        });
+    getBroker.then(function (broker) {
         /**
          *  msg: {
          *      CreateTime: Number: Date#getTime() milliseconds
@@ -68,7 +23,7 @@ function callback(){
          *      NewStatus: String
          *  }
          */
-        broker.brokerNodeManager.onAgentManagerStatusChange(function(err, data){
+        broker.brokerNodeManager.onAgentManagerStatusChange(function (err, data) {
             console.log("[system]: agent status manager changed [agentId]: data.NodeId " + data.NodeId);
             console.log(data);
             app.nodeManager.nodeMaps[data.NodeId] = {
@@ -94,7 +49,7 @@ function callback(){
          *      NewStatus: String
          *  }
          */
-        broker.brokerNodeManager.onAgentStatusChange(function(err, data){
+        broker.brokerNodeManager.onAgentStatusChange(function (err, data) {
             console.log("[system]: agent status changed [agentId]: data.AgentId" + data.AgentId);
             console.log(data);
             app.nodeManager.agentMaps[data.AgentId] = {
@@ -103,48 +58,66 @@ function callback(){
                 agentStatus: data.NewStatus
             };
         });
-        broker.brokerNodeManager.onAgentStart(function(err, data){
-            console.log("[system]: agent start [agentId]: data.AgentId" + data.AgentId);
-            console.log(data);
-            /**
-             *  msg: {
-             *      CreateTime: Number: Date#getTime() milliseconds
-             *      AgentId: String
-             *      Intention: String 'register' | 'login'
-             *      Mode: 'trusted' | 'untrusted'
-             *      Nickname: String  ONLY applicable if Mode is untrusted
-             *      Sex: 0 1 2        ONLY applicable if Mode is untrusted
-             *      Region:           ONLY applicable if Mode is untrusted
-             *
-             *  }
-             */
-            broker.brokerNodeManager.startRequest({
-                NodeId: app.nodeManager.getNode(),
-                CreateTime: (new Date()).getTime(),
-                AgentId: data.AgentId,
-                Intention: 'login',
-                Mode: 'trusted'
-            })
-        });
 
-        //test
-        broker.brokerAgent.statusResponse({
-            CreateTime: (new Date()).getTime(),
-            AgentId: 'test1',
-            Intention: 'login',
-            Mode: 'trusted' | 'untrusted',
-            Nickname: 'hehe',
-            Sex: 1,
-            Region: 'zhonggo'
+        /**
+         *  Message routing: bot ---> node ( ---> am)
+         *
+         *  msg: {
+         *      CreateTime: Number: Date#getTime() milliseconds
+         *      AgentId: String
+         *      Command: 'start'
+         *      Intention: String 'register' | 'login'
+         *      Mode: 'trusted' | 'untrusted'
+         *      Nickname: String  ONLY applicable if Mode is untrusted
+         *      Sex: 0 1 2        ONLY applicable if Mode is untrusted
+         *      Region:           ONLY applicable if Mode is untrusted
+         *  }
+         *
+         *  msg: {
+         *      CreateTime: Number: Date#getTime() milliseconds
+         *      AgentId: String
+         *      Command: 'stop'
+         *      Mode: 'graceful' | 'ungraceful'
+         *          graceful means stop until all action messages,
+         *          ungraceful means stop it right now whatever unhandled action messages there.
+         *  }
+         *
+         *  msg: {
+         *      CreateTime: Number: Date#getTime() milliseconds
+         *      AgentId: String
+         *      Command: 'reload'
+         *      Mode: 'graceful' | 'ungraceful'
+         *          graceful means reload until all action messages,
+         *          ungraceful means reload it right now whatever unhandled action messages there.
+         *  }
+         *
+         */
+        broker.brokerNodeManager.onCommand(function (err, data) {
+            if (data.Command === 'start') {
+                console.log("[system]: agent start [agentId]: data.AgentId" + data.AgentId);
+                console.log(data);
+
+                /**
+                 *  msg: {
+                 *      CreateTime: Number: Date#getTime() milliseconds
+                 *      NodeId: String
+                 *      AgentId: String
+                 *      Command: 'start'
+                 *      Intention: String 'register' | 'login'
+                 *      Mode: 'trusted' | 'untrusted'
+                 *      Nickname: String  ONLY applicable if Mode is untrusted
+                 *      Sex: 0 1 2        ONLY applicable if Mode is untrusted
+                 *      Region:           ONLY applicable if Mode is untrusted
+                 *  }
+                 **/
+                broker.brokerNodeManager.command({
+                    NodeId: app.nodeManager.getNode(),
+                    CreateTime: (new Date()).getTime(),
+                    AgentId: data.AgentId,
+                    Intention: 'login',
+                    Mode: 'trusted'
+                }, data.agentId)
+            }
         });
-        broker.brokerBot.agentStart({
-            CreateTime: (new Date()).getTime(),
-            AgentId: 'test2',
-            Intention: 'login',
-            Mode: 'trusted' | 'untrusted',
-            Nickname: 'hehe',
-            Sex: 1,
-            Region: 'zhonggo'
-        })
     });
 }
